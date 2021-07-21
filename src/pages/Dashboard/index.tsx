2d4384f683b9f071d813e263791039653e8470ae
Andrey Masiero <andreymasiero@gmail.com>
2021-07-21 17:50:48 -0300

    User creation through UI form done.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { useAuth } from '../../hooks/auth';
+const Dashboard: React.FC = () => {
+	const { signOut } = useAuth();
+	return <h1 onClick={() => signOut()}>Dashboard</h1>;
+};
 
 export default Dashboard;
